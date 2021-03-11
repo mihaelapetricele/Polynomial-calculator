fabe2c63e93d02cffaaf24fbd09a93d2e3acc33d
Mihaela Petricele <mihaelapetricele@yahoo.com>
2021-03-11 11:50:46 +0200

    initial commit

---
+++ src/main/java/ro/tuc/tp/App.java
@@ -0,0 +1,13 @@
+package ro.tuc.tp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
